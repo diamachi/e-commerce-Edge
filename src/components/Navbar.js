1d3dcd3de458fa3103fd7e8495a327c36f84ac82
mohit385 <mdayal385@gmail.com>
2021-10-30 02:21:14 +0530

    Cleaned code & fixed nav
    navbar completed.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,40 +7,32 @@ function Navbar() {
         <>
         {/* logo part */}
             <nav className="main-nav">
-                <div classname="logo">
-                    <h2>
-                        <span>M</span>yntra
-                    </h2>
-                </div>
-            {/* 2nd menus */}
-            <div className="menu-link">
-                <ul>
-                    <li>
+                <ul className="nav-list">
+                   <li className="list-item logo">
+                   <a href="#">Myntra</a>
+                    </li>
+                    <li className="list-item">
                         <a href="#">Home</a>
                     </li>
-                    <li>
+                    <li className="list-item">
                         <a href="#">New Arrivals</a>
                     </li>
-                    <li>
+                    <li className="list-item">
                         <a href="#">Rewards</a>
                     </li>
-                    <li>
+                    <li className="list-item">
                         <a href="#">Contact</a>
                     </li>
                       {/* search icon */}
-                      <div className="search-bar">
+                      <li className="list-item search-bar">
                       <SearchIcon/>
-                      <input type="text"/>
-                      </div>
+                      <input type="text" placeholder="Search"/>
+                      </li>
+                      
                 </ul>
               
-            </div>
             </nav>
-
             {/* Chat sec */}
-            <section className="chat-section">
-            <h1>lets Chat here</h1>
-            </section>
         </>
     )
 }
