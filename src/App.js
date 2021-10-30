554bfd213313184f7a25d9ec824557caad38631c
Peeyush Yadav <peeyushadav1402@gmail.com.com>
2021-10-30 21:23:23 +0530

    Card & Sidebar(no trigger)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Card from './components/Card'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-   {/* navbar */}
-   <Navbar/>
-   {/* sidebar */}
-   {/* genchat */}
+      {/* navbar */}
+      <Navbar />
+      <Card />
+      {/* sidebar */}
+      {/* genchat */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import "./Navbar.css";
 import SearchIcon from '@material-ui/icons/Search';
@@ -5,11 +6,11 @@ import SearchIcon from '@material-ui/icons/Search';
 function Navbar() {
     return (
         <>
-        {/* logo part */}
+            {/* logo part */}
             <nav className="main-nav">
                 <ul className="nav-list">
-                   <li className="list-item logo">
-                   <a href="#">Myntra</a>
+                    <li className="list-item logo">
+                        <a href="#">Myntra</a>
                     </li>
                     <li className="list-item">
                         <a href="#">Home</a>
@@ -23,14 +24,14 @@ function Navbar() {
                     <li className="list-item">
                         <a href="#">Contact</a>
                     </li>
-                      {/* search icon */}
-                      <li className="list-item search-bar">
-                      <SearchIcon/>
-                      <input type="text" placeholder="Search"/>
-                      </li>
-                      
+                    {/* search icon */}
+                    <li className="list-item search-bar">
+                        <SearchIcon />
+                        <input type="text" placeholder="Search" />
+                    </li>
+
                 </ul>
-              
+
             </nav>
             {/* Chat sec */}
         </>
