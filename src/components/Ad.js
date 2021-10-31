a01c59797fcd18f8ed63950aa1f43d795c79c3f0
Peeyush Yadav <peeyushadav1402@gmail.com.com>
2021-10-31 22:26:51 +0530

    Added placeholder for Adverts

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Advertisement } from 'semantic-ui-react'
+import './Ad.css'
 
 const Adver = () => (
-    <div>
-        <Advertisement unit='leaderboard' test='Leaderboard' />
-        <Advertisement unit='large rectangle' test='Large Rectangle' />
-        <Advertisement unit='half page' test='Half Page' />
+    <div class='bruh'>
+        <Advertisement unit='half page' test='Placeholder For Ads' />
+
     </div>
+
+
 )
 
 export default Adver
