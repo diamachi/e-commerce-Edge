cd1744a697251ff4bb878916242bb2fd282d5343
nitin06890 <nitin06890@gmail.com>
2021-11-04 00:19:00 +0530

    changes

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -3,9 +3,9 @@ import { Advertisement } from 'semantic-ui-react'
 import './Ad.css'
 
 const Adver = () => (
-    <div class='adver'>
+    <div class='bruh'>
         <Advertisement unit='half'>
-            <img src="src\images\Banner.png" alt='Advertisment Banner' />
+            <img src='https://i.imgur.com/NPvi5nY.png' alt='mah swag' />
         </Advertisement>
     </div>
 
