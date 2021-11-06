a75b9b0f9bb2ab3bb7764a5e009fcee1a3c2f9ba
mohit385 <mdayal385@gmail.com>
2021-11-06 21:22:51 +0530

    rewards profile card done.

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import "./Rewards.css";
+import pic from "./peeyush.png";
 
 function Rewards() {
-    return (
-        <div>
-            Rewards site
+  return (
+    <div className="profile">
+      <div className="profile-card">
+        <div className="card-header">
+          <div className="pic">
+            <img src={pic} alt="" />
+          </div>
+          <div className="name">Peeyush Yadav</div>
+          <div className="desc">Hemllooooooo!</div>
+          <div className="sm">
+            <a href="#" className="fab fa-facebook-f"></a>
+            <a href="#" className="fab fa-instagram"></a>
+            <a href="#" className="fab fa-whatsapp"></a>
+            <a href="#" className="fab fa-twitter"></a>
+          </div>
+          <a href="#" className="contact-btn">
+            Change profile
+          </a>
         </div>
-    )
+        <div className="card-footer">
+          <div className="numbers">
+            <div className="item">
+              <span>120</span> Posts
+            </div>
+            <div className="item">
+              <span>69</span> Answered
+            </div>
+            <div className="item">
+              <span>127</span> Points
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Rewards
+export default Rewards;
