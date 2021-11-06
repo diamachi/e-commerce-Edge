a692e27c5f2c8c1ff972e5a3a7793545ef88d0bb
mohit385 <mdayal385@gmail.com>
2021-11-07 02:45:19 +0530

    forumlists added

--- src/components/Community.js
+++ src/components/Community.js
@@ -2,15 +2,36 @@ import React from "react";
 import Order from "./Order";
 import SideCard from "./SideCard";
 import "./Community.css";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { ForumLists } from "./ForumLists";
 
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 function Community() {
   return (
-    <div>
+    <div className="community-main">
       {/* <Order /> */}
       <div className="sidecards-section">
         <SideCard />
         <SideCard />
         <SideCard />
+      </div>
+      <div className="forums-section">
+        <p>
+          View all community forums{" "}
+          <span>
+            <KeyboardArrowDownIcon />
+          </span>
+        </p>
+        <div>
+          <ForumLists />
+        </div>
       </div>
     </div>
   );
