62a7b5ca5a66a31f2260fdee140cebe547bd792a
mohit385 <mdayal385@gmail.com>
2021-11-07 21:44:15 +0530

    logo edited.

--- src/components/Upper.js
+++ src/components/Upper.js
@@ -1,32 +1,68 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Upper.css";
-import {
-  Nav,
-  Container,
-  Navbar, Form, FormControl
-} from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Nav, Container, Navbar, Form, FormControl } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Upper() {
   return (
     <>
       <Navbar sticky="top" bg="light" expand={false} className="color-nav">
         <Container fluid>
-          <Navbar.Brand className="gradient-text logo" componentClass={Link} to="/" href="/"><img class="logo-image"
-            src="https://i.imgur.com/PIVn72N.png"
-            width="40"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
-            {' '}Forum</Navbar.Brand>
+          <Navbar.Brand
+            className="gradient-text logo"
+            componentClass={Link}
+            to="/"
+            href="/"
+          >
+            <img
+              class="logo-image"
+              src="https://i.imgur.com/PIVn72N.png"
+              width="40"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />{" "}
+            -Forum
+          </Navbar.Brand>
 
-          <Nav.Link class="nav-list  " componentClass={Link} href="/community" to="/community"><h3><p class="gradient-text">Community</p></h3></Nav.Link>
-          <Nav.Link class="nav-list  " componentClass={Link} href="/community" to="/community"><h3><p class="gradient-text">New Arrivals</p></h3></Nav.Link>
-          <Nav.Link class="nav-list  " componentClass={Link} href="/rewards" to="/rewards"><h3><p class="gradient-text">Rewards</p></h3></Nav.Link>
+          <Nav.Link
+            class="nav-list  "
+            componentClass={Link}
+            href="/community"
+            to="/community"
+          >
+            <h3>
+              <p class="gradient-text">Community</p>
+            </h3>
+          </Nav.Link>
+          <Nav.Link
+            class="nav-list  "
+            componentClass={Link}
+            href="/community"
+            to="/community"
+          >
+            <h3>
+              <p class="gradient-text">New Arrivals</p>
+            </h3>
+          </Nav.Link>
+          <Nav.Link
+            class="nav-list  "
+            componentClass={Link}
+            href="/rewards"
+            to="/rewards"
+          >
+            <h3>
+              <p class="gradient-text">Rewards</p>
+            </h3>
+          </Nav.Link>
           <Form className="d-flex">
-            <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
           </Form>
         </Container>
       </Navbar>
