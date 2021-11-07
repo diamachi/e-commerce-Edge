0f168be3e6ffb949683c92ab67a8aa637f2b9079
nitin06890 <nitin06890@gmail.com>
2021-11-07 20:44:23 +0530

    logo added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import "./Login.css";
-import logosrc from '../images/jiscord.png';
+import logosrc from '../images/myntra_logo.png';
 import { Button } from '@material-ui/core';
 import { auth, provider } from '../firebase';
+
 
 function login() {
     const signIn=() => {
@@ -11,11 +12,11 @@ auth.signInWithPopup(provider)
     };
     return (
         <div className="login">
-          <h2>I am Login</h2>  
+          {/* <h2>Myntra Forum</h2>   */}
           <div className="login_logo">
           <img src={logosrc} alt="" />
           </div>
-          <Button onClick={signIn}>Sign In</Button>
+          <Button onClick={signIn}>Login Now</Button>
         </div>
     );
 }
