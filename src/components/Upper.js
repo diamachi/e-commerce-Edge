ca6a34e2028287cd9be472cde9f2826348243eef
Peeyush Yadav <peeyushadav1402@gmail.com.com>
2021-11-07 16:38:03 +0530

    removed hamburger

--- src/components/Upper.js
+++ src/components/Upper.js
@@ -22,43 +22,7 @@ function Upper() {
           <Nav.Link class="nav-list  " componentClass={Link} href="/community" to="/dogs">Community</Nav.Link>
           <Nav.Link class="nav-list  " componentClass={Link} href="/community" to="/dogs">New Arrivals</Nav.Link>
           <Nav.Link class="nav-list  " componentClass={Link} href="/rewards" to="/dogs">Rewards</Nav.Link>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">
-                Offcanvas
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Community</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
+
         </Container>
       </Navbar>
     </>
