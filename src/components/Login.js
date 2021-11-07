4392c438806fbf5872a48c9fc6905c4d2459f69a
mohit385 <mdayal385@gmail.com>
2021-11-07 21:37:59 +0530

    Styles and Fonts changed.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./Login.css";
-import logosrc from '../images/myntra_logo.png';
-import { Button } from '@material-ui/core';
-import { auth, provider } from '../firebase';
-
+import logosrc from "../images/myntra_logo.png";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
 
 function login() {
-    const signIn=() => {
-auth.signInWithPopup(provider)
-.catch(error => alert(error.message));
-    };
-    return (
-        <div className="login">
-          {/* <h2>Myntra Forum</h2>   */}
-          <div className="login_logo">
-          <img src={logosrc} alt="" />
-          </div>
-          <Button onClick={signIn}>Login Now</Button>
-        </div>
-    );
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
+  return (
+    <div className="login">
+      {/* <h2>Myntra Forum</h2>   */}
+      <div className="login_logo">
+        <img src={logosrc} alt="" />
+        <span className="title">-Forum</span>
+      </div>
+      <Button onClick={signIn}>Login Now</Button>
+    </div>
+  );
 }
 
 export default login;
